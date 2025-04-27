48feaffc0548e5a620c89637d7f6a269657b0ba2
AKuchko <vasco_da_gama1469@mail.ru>
2025-04-27 23:40:49 +0700

    feat: Base button component

---
+++ components/base/button.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    class="base-button"
+    :class="rootCSSClasses"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { CSSClasses } from '~/types';
+
+interface ButtonProps {
+  outlined?: boolean;
+}
+
+const { outlined } = defineProps<ButtonProps>()
+
+const rootCSSClasses = computed((): CSSClasses => ({
+  'base-button--outllined': outlined,
+}))
+</script>
+
+<style>
+.base-button {
+  height: 56px;
+  min-width: 200px;
+  border-radius: 55px;
+
+  font-size: 18px;
+  font-weight: 700;
+
+  color: var(--color-white);
+  background: var(--color-main);
+
+  transition: var(--transition);
+}
+
+.base-button:hover {
+  background: var(--color-main-dark);
+}
+
+.base-button--outlined {
+  border: 1.4px solid var(--color-main);
+  color: var(--color-main);
+  background: transparent;
+}
+
+.base-button--outlined:hover {
+  color: var(--color-white);
+  background: var(--color-main);
+}
+</style>
