0a867ded4ad168e89114cfda0236706dd86160c9
AKuchko <vasco_da_gama1469@mail.ru>
2025-04-27 23:41:00 +0700

    feat: BaseCard component

---
+++ components/base/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="base-card">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+.base-card {
+  border-radius: 20px;
+  box-shadow: 10px 40px 50px 0 rgba(229, 233, 246, 0.4);
+  background: var(--color-white);
+}
+</style>
