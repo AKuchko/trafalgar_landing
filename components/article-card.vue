2ac6fdeec200e71a8e99fb211d46654530767269
AKuchko <vasco_da_gama1469@mail.ru>
2025-04-29 22:51:58 +0700

    feat: Add article-card component

---
+++ components/article-card.vue
@@ -0,0 +1,88 @@
+<template>
+  <base-card class="article-card">
+    <NuxtImg
+      class="article-card__image"
+      :src="props.image.url"
+      :width="props.image.width"
+      :height="props.image.height"
+    />
+    <div class="article-card__body">
+      <h4 class="article-card__title">{{ props.title }}</h4>
+      <p class="article-card__description">
+        {{ props.description }}
+      </p>
+      <NuxtLink
+        class="article-card__link"
+        :to="props.link"
+      >
+        Read more
+        <icon-arrow />
+      </NuxtLink>
+    </div>
+  </base-card>
+</template>
+
+<script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router';
+import type { ImageInfo } from '~/types';
+
+interface ArticleCardProps {
+  image: ImageInfo;
+  title: string;
+  description: string;
+  link: RouteLocationRaw;
+}
+
+const props = defineProps<ArticleCardProps>();
+</script>
+
+<style>
+.article-card {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.article-card__body {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 24px 32px 35px;
+}
+
+.article-card__title {
+  margin-bottom: 12px;
+  font-weight: 700;
+  font-size: 21px;
+  line-height: 32px;
+  color: var(--color-black);
+}
+
+.article-card__description {
+  margin-bottom: 25px;
+  font-weight: 300;
+  line-height: 28px;
+  color: var(--color-gray);
+}
+
+.article-card__link {
+  display: flex;
+  align-items: center;
+  gap: 14px;
+
+  margin-top: auto;
+
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 28px;
+
+  color: var(--color-main);
+}
+
+.article-card__link svg {
+  width: 14.2px;
+  height: 9px;
+
+  margin-top: 1.5px;
+}
+</style>
---
+++ components/icon/arrow.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 15 9">
+    <g clip-path="url(#a)">
+      <path fill="currentColor" d="M.8 5.3h10.6L9.1 7.6c-.3.4-.3.8 0 1.2.3.3.8.3 1.1 0L14 5c.3-.4.3-.8 0-1.2L10.2.2a.8.8 0 0 0-1.1 0c-.3.4-.3.8 0 1.2l2.3 2.3H.8c-.4 0-.8.3-.8.8s.4.8.8.8Z"/>
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M14.1 9H.1V0h14z"/>
+      </clipPath>
+    </defs>
+  </svg>
+</template>
